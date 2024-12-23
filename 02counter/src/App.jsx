91416236cee0737dd0d6058ab5166899c5b45c8b
vivi2004 <vivekpurbey586@gmail.com>
2024-12-23 15:55:13 +0530

    A basic react counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+ const [ counter, setCounter] = useState(12);
+
+
+  const addValue = () => {
+   setCounter(counter + 1);  
+  }
+  const removeValue = () => {
+    setCounter(counter -1 );
+  }
+  return (
+    <>
+      <h1>Learn React{counter}</h1>
+      <h2> Counter Value : {counter}</h2>
+
+      <button
+        onClick={addValue}
+      > Add value </button> {" "}
+      <button
+        onClick={removeValue} 
+      > Remove value</button>
+
+      <p> footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
