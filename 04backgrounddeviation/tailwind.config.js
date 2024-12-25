a00dc93a30a6e83f7b967dd727fc621e5f00d92f
vivi2004 <vivekpurbey586@gmail.com>
2024-12-25 18:33:09 +0530

    Changes  successfully

--- 04backgrounddeviation/tailwind.config.js
+++ 04backgrounddeviation/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
