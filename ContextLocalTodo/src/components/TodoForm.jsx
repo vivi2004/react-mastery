aebd51e2f362db28ab88cba03cdcc321d068faeb
vivi2004 <vivekpurbey586@gmail.com>
2025-01-23 23:05:43 +0530

    Modern Todo with react successfully executedd

---
+++ ContextLocalTodo/src/components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+function TodoForm() {
+    // const [todos, setTodos] = useState([])
+    const [todoMsg, setTodoMsg] = useState("");
+    const { addTodo } = useTodo();
+
+    const add = (e) => {
+        e.preventDefault();
+        if (!todoMsg.trim()) return;
+
+        addTodo({ todo: todoMsg, completed: false });
+        setTodoMsg("");
+    };
+
+    return (
+        <form onSubmit={add} className="flex">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 
+                rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={todoMsg}
+                onChange={(e) => setTodoMsg(e.target.value)}
+            />
+            <button
+                type="submit"
+                className="rounded-r-lg px- py-1 bg-green-600 text-white shrink-0"
+            >
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
